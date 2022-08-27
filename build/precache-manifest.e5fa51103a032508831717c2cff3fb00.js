cc69981638980e1d90d3c56ad2b95dcf11e480f2
siyam haider <i211571@nu.edu.pk>
2022-08-26 23:14:59 -0400

    initial commit

---
+++ build/precache-manifest.e5fa51103a032508831717c2cff3fb00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9540fa1e7cc893b97ada41b11716e8ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d60973b32a40a086082",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "526ce9b2fd420187ca4b",
+    "url": "/static/js/2.36071d3d.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.36071d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d60973b32a40a086082",
+    "url": "/static/js/main.a6dc07b8.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
